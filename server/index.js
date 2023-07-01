9ec3770710c1af33adefa5373cc3813109cc17d0
ranoodles <115419765+ranoodles@users.noreply.github.com>
2023-06-30 22:16:46 -0700

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const {createConnection} = require('mysql');
+
+const app = express();
+const db = createConnection({
+    host:"mysql.biomath.dreamhosters.com",
+    user:"biomath",
+    password:"AroraLagu2023",   
+    database:"biomath"
+})
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+});
+
+app.get('/test', (req, res) => {
+    const q = "Select * FROM biomathusers";
+    console.log("log: show table");
+    db.query(q, (err, data) => {
+        if (err) return res.json(err);
+        return res.json(data);
+    })
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
