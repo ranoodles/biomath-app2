2cf0750e9ead51f8ba92c76000979997064c2fb2
alags21 <68449631+alags21@users.noreply.github.com>
2023-07-04 00:22:13 -0400

    landing edits

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Nav from './website-constants/NavBarLoggedOut'
-import { Box, Button, Typography, Container, Stack, Divider, Grid, Card } from '@mui/material';
+import { Paper, Box, Button, Typography, Container, Stack, Divider, Grid, Card } from '@mui/material';
 import styled from 'styled-components';
 import {
   createTheme,
@@ -17,11 +17,15 @@ import {
 } from "react-router-dom"
 import theme from "./website-constants/Theme.jsx"
 import { motion as threeDMotion } from "framer-motion-3d"
+import Image from 'mui-image'
+
+
 const slides = [
   "https://res.cloudinary.com/demo/image/upload/v1312461204/sample.jpg",
   "https://res.cloudinary.com/demo/basketball_shot.jpg",
   "https://i.ytimg.com/vi/PCwL3-hkKrg/maxresdefault.jpg"
 ]
+
 
 const Root = styled('div')(({ theme }) => ({
 
@@ -111,9 +115,9 @@ const SplitBoxesGrid = styled(Grid)`
     align-items: center;
     font-size: 25px;
     ${'' /* height: 70vh; */}
-    gap: 2vw;
-    padding-top: 2vw;
-    padding-bottom: 2vw;
+    gap: 4vw;
+    padding-top: 7rem;
+    padding-bottom: 0vw;
   }
 `;
 
@@ -146,7 +150,8 @@ const AppBenefitsHolderGrid = styled(Grid)`
     gap: 2vw;
     padding-top: 2vw;
     padding-bottom: 2vw;
-    margin-top: 10rem;
+    margin-top: 1rem;
+    gap: 4vw;
   }
 `;
 
@@ -167,7 +172,7 @@ const AppBenefitsStack = styled(Stack)`
     display: flex;
     justify-content: center;
     align-items: center;
-    padding: 5rem 5rem 5rem 5rem;
+    padding: 2rem 5rem 2rem 5rem;
     
     }
 `;
@@ -179,6 +184,52 @@ const BenefitDescriptionText = styled(Typography)`
     align-items: center;
 }
 `;
+
+const OurMissionGrid = styled(Grid)`
+  && {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 0rem 0rem 0rem 5rem;
+}
+`
+
+const OurMissionTextGrid = styled(Grid)`
+  && {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+}
+`
+
+const OurMissionImgGrid = styled(Grid)`
+  && {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+}
+`
+const OurMissionImg = styled(Image)`
+  && {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 20px;
+  }
+`
+
+const OurMissionText = styled(Typography)`
+  && {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-weight: 100;
+    padding: 3rem;
+    text-align: center;
+    color: white;
+    text-align: left;
+  }
+`
 
 const AppBenefitsText = [
   {
@@ -233,7 +284,7 @@ export default function Landing() {
         <TitleStack 
             spacing={2} 
             component={motion.div} 
-            initial={{opacity:0, scale:0.2}} 
+            initial={{opacity:0, scale:0.5}} 
             animate={{ opacity:1, scale:1}} 
             transition={{duration: 3, ease:"easeInOut"}}>
           <Box component="span" sx={{ p: 5 }} >
@@ -252,11 +303,11 @@ export default function Landing() {
           <SignupButtonTop variant="contained" onClick={() => {navigate('/signup')}}>Sign Up</SignupButtonTop>
         </TitleStack>
       </Box>
-    
+    {/* ========================================================================================================================================== */}
     {/* Our Courses */}
       <SplitBoxesGrid container divider={<Divider orientation="vertical" flexItem />} direction="row">
       {CourseDescriptionText.map((CourseCard) => (
-        <SubjectDescriptionGrid item xs={11} sm={5.75} sx={{backgroundColor:"skyblue"}}
+        <SubjectDescriptionGrid item xs={11} sm={5} sx={{backgroundColor:"skyblue"}}
         component={motion.div} 
         initial={{ opacity: 0, scale: 0 }}
         whileInView=
@@ -278,36 +329,57 @@ export default function Landing() {
         </SubjectDescriptionGrid>
   ))}
       </SplitBoxesGrid>
+      {/* <Divider color='white' /> */}
+{/* ========================================================================================================================================== */}
+      {/* Our Mission */}
+      <BiomathText variant="h3" color="white" paddingTop='7rem' >Our Mission</BiomathText>
+      <OurMissionGrid container>
+        <OurMissionImgGrid Item xs={11} sm={4}>
+            <OurMissionImg src="https://images.unsplash.com/photo-1532094349884-543bc11b234d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80"
+            alt="Scientist" />
+          </OurMissionImgGrid>
+        <OurMissionTextGrid Item xs={11} sm={5}>
+          <OurMissionText variant="h5">
+            Deserunt reprehenderit aute amet Lorem laborum irure. Dolor eu pariatur incididunt dolore aute minim qui est et tempor velit elit. Sint sit duis nulla esse dolor. Id officia do dolore aliqua exercitation in culpa ad voluptate ullamco eu ex cupidatat. Cillum nulla eu labore quis adipisicing mollit ad velit cupidatat.
+          </OurMissionText>
+        </OurMissionTextGrid>
+        
+      </OurMissionGrid>
+{/* ========================================================================================================================================== */}
+      {/* <Divider color='white' /> */}
+      
+      
       {/* 3 Boxes */}
+      <BiomathText variant="h3" color="white" paddingTop='7rem' >Benefits at a Glance</BiomathText>
       <AppBenefitsHolderGrid container>
         {AppBenefitsText.map((benefitCard) => (
           <AppBenefitsGrid item
         xs={11} 
-        sm={3} 
-        sx={{backgroundColor:"skyblue", borderRadius:20}} 
+        sm={3.3} 
+        sx={{backgroundColor:"skyblue", borderRadius:10}} 
         component={motion.div}
-        initial={{ opacity: 0, scale: 1 }}
+        initial={{ opacity: 0, scale: 1.5 }}
         whileInView=
         {{ opacity: 1, scale: 1 }}
         transition={{
         duration: 0.8,
         delay: 0,
-        ease: [0, 0.71, 0.2, 1.01]
+        ease: [0, 0.71, 0.2, 1.01],
+        
       }}
         whileHover=
         {{ scale: 1.05
         }}
         >
           <AppBenefitsStack>
-            <BiomathText>{benefitCard.title}</BiomathText>
+            <BiomathText variant="h4">{benefitCard.title}</BiomathText>
             <BenefitDescriptionText>{benefitCard.description}</BenefitDescriptionText>
           </AppBenefitsStack>
         </AppBenefitsGrid>
         ))}
 
       </AppBenefitsHolderGrid>
-
-
+{/* ========================================================================================================================================== */}
 
       {/* Start Journey Today */}
       <Box sx={{ height: '40vh', display:"flex", alignItems:"center", justifyContent:"center"}}>
