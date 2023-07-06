5bdceabd92d564307a385f9add6284c9d9d1022e
ranoodles <115419765+ranoodles@users.noreply.github.com>
2023-07-05 21:31:07 -0700

    imagetextshtuff

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import React from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import theme from "./pages/website-constants/Theme.jsx";
 import AppliedMathPage from "./pages/appliedmath";
+import ImgText from "./pages/website-constants/ImageText"
 
 // const theme = createTheme({
 //   typography: {
@@ -36,6 +37,7 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/biotechnology" element={<BiotechnologyPage />} />
             <Route path="/appliedmath" element={<AppliedMathPage />} />
+            <Route path="/imgtext" element={<ImgText title="Subheading" img="https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png" text="Velit enim occaecat exercitation Lorem Lorem aliqua reprehenderit magna duis eiusmod nisi. Lorem voluptate duis occaecat pariatur fugiat excepteur minim. Elit Lorem dolor voluptate eu veniam mollit aliquip." />} />
           </Routes>
         </BrowserRouter>
       </div>
