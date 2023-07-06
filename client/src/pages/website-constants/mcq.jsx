52e16d6cd415569df9d0c19a39beff20fd11ca0c
alags21 <68449631+alags21@users.noreply.github.com>
2023-07-05 22:19:19 -0400

    made mcq

---
+++ client/src/pages/website-constants/mcq.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import {
+  Box,
+  Button,
+  Typography,
+  Container,
+  Stack,
+  Divider,
+  Grid,
+  ButtonGroup,
+} from "@mui/material";
+import { motion } from "framer-motion";
+import theme from "./Theme";
