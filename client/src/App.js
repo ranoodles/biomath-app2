df6a6f0e53028af82452f8492ab69bc3941ed94c
ranoodles <115419765+ranoodles@users.noreply.github.com>
2023-09-04 18:48:49 -0700

    refreshErrorRemoved

--- client/src/App.js
+++ client/src/App.js
@@ -12,21 +12,20 @@ const Login = lazy(() => import("./pages/Login"));
 const BiotechnologyPage = lazy(() => import("./pages/biotechnology"));
 const AppliedMathPage = lazy(() => import("./pages/appliedmath"));
 
-const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-
 function App() {
   const [loggedIn, setLoggedIn] = useState(null);
+  // const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     // document.body.style.overflow = "hidden";
     const fetchLoginStatus = async () => {
       try {
-        const res = await axios.get("http://localhost:8001/fetchCurrentUser", {
+        const res = await axios.get("http://localhost:8001/isLoggedIn", {
           withCredentials: true,
           credentials: "include"
         });
         const user = res.data;
-        setLoggedIn(user != null ? true : false);
-        // setLoggedIn(false)
+        setLoggedIn(user);
       } catch (err) {
         console.log("im in app js", err);
       }
@@ -34,25 +33,10 @@ function App() {
     fetchLoginStatus();
   }, []);
 
-  if (loggedIn === null) {
-    // return <div>Loading...</div>;
-    // console.log("Loading start...")
-    // async function test() {
-    //   await sleep(5000);
-    // }
-    // test();
-    // console.log("Loading end...")
-    // function sleep(ms) {
-    //   const start = Date.now();
-    //   while (Date.now() - start < ms) {
-    //     // This loop will block the thread
-    //   }
-    // }
-    // console.log("Before sleep");
-    // sleep(10000);
-    // console.log("After sleep");
-  }
-  
+  // if (isLoading) {
+  //   return <div>Loading...</div>; // Show loading indicator
+  // }
+
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
@@ -60,56 +44,36 @@ function App() {
           <Suspense fallback={<div className="container">Loading...</div>}>
             <Routes>
               <Route
-                // key="login"
                 path="/login"
-                element={
-                  loggedIn ? (
-                    <Navigate to="/courses" />
-                  ) : (
-                    <Login />
-                  )
-                }
+                element={loggedIn === true ? <Navigate to="/courses" /> : loggedIn === false ? <Login /> : <div>Loading...</div>}
               />
               <Route
                 path="/biotechnology/:lessonid"
-                element={
-                  loggedIn ? <BioLearningPath /> : <Navigate to="/login" />
-                }
+                element={loggedIn === true ? <BioLearningPath /> : loggedIn === false ? <Navigate to="/login" /> : <div>Loading...</div>}
               />
               <Route
                 path="/courses"
-                element={loggedIn ? <Courses /> : <Navigate to="/login" />}
+                element={loggedIn === true ? <Courses /> : loggedIn === false ? <Navigate to="/login" /> : <div>Loading...</div>}
               />
               <Route
                 path="/appliedmath/:lessonid"
-                element={
-                  loggedIn ? <MathLearningPath /> : <Navigate to="/login" />
-                }
+                element={loggedIn === true ? <MathLearningPath /> : loggedIn === false ? <Navigate to="/login" /> : <div>Loading...</div>}
               />
               <Route
-                key="landing"
                 path="/"
-                element={!loggedIn ? <Landing /> : <Navigate to="/courses" />}
-                // element = {loggedIn === null ? <Landing></Landing>}
+                element={loggedIn === false ? <Landing /> : loggedIn === true ? <Navigate to="/courses" /> : <div>Loading...</div>}
               />
               <Route
-                key="signup"
                 path="/signup"
-                element={!loggedIn ? <Signup /> : <Navigate to="/courses" />}
+                element={loggedIn === false ? <Signup /> : loggedIn === true ? <Navigate to="/courses" /> : <div>Loading...</div>}
               />
               <Route
-                key="biotechnology"
                 path="/biotechnology"
-                element={
-                  loggedIn ? <BiotechnologyPage /> : <Navigate to="/login" />
-                }
+                element={loggedIn === true ? <BiotechnologyPage /> : loggedIn === false ? <Navigate to="/login" /> : <div>Loading...</div>}
               />
               <Route
-                key="appliedmath"
                 path="/appliedmath"
-                element={
-                  loggedIn ? <AppliedMathPage /> : <Navigate to="/login" />
-                }
+                element={loggedIn === true ? <AppliedMathPage /> : loggedIn === false ? <Navigate to="/login" /> : <div>Loading...</div>}
               />
             </Routes>
           </Suspense>
